3c2a279a03e13f8f18a0a53a971c4588ceeb22b8
Gabriel-Schiestl <gabriel.schiestl25@hotmail.com>
2024-10-29 15:23:40 -0300

    feat: adiciona manipulacao de imagem

--- packages/IA/server/main.py
+++ packages/IA/server/main.py
@@ -1,14 +1,19 @@
 from flask import Flask, request
 import os
 import json
+from PIL import Image
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def predict():
-    data = request.get_json()
-    return 'Ola'
+    if 'image' not in request.files:
+        return 'Nenhuma imagem enviada', 400
+    image_file = request.files['image']
+    image = Image.open(image_file)
+    image = image.resize((224, 224))
+    
 
 
 if __name__ == '__main__':

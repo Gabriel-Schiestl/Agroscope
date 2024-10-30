d08457e86294d826d42ced1ab9f846efa91ef893
Gabriel-Schiestl <gabriel.schiestl25@hotmail.com>
2024-10-29 21:52:34 -0300

    feat: implementa dockerFiles

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import Image from "next/image";
 import { ChangeEvent, ReactEventHandler, useState } from "react";
 
@@ -16,13 +17,32 @@ export default function Home() {
     }
   };
 
+  const onClick = async () => {
+    if (!file) return;
+    const formData = new FormData();
+    formData.append("image", file);
+    try {
+      const result = await axios.post(
+        "http://localhost:5000/predict",
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      console.log(result.data);
+    } catch (error) {
+      console.error("Erro ao enviar a imagem:", error);
+    }
+  };
+
   return (
     <div className="w-full h-screen flex justify-center items-center bg-slate-400">
-      <div className="w-1/2 h-2/3 bg-white rounded-md flex justify-start p-6 items-center flex-col">
+      <div className="w-1/2 bg-white rounded-md flex justify-start p-6 items-center flex-col">
         <div className="mb-8">
           <input type="file" accept="image/*" onChange={onChange} />
         </div>
         {url && <Image src={url} alt="Teste" width={500} height={500} />}
+        <button onClick={onClick}>Predict</button>
       </div>
     </div>
   );
